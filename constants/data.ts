f2bb8e17b71a9de51d2e9e8e7b4570e66e9965f8
rakibhassannayem <rakibhassannayem@gmail.com>
2026-06-04 11:57:48 +0600

    feat: project setup, nativewind, tab navigation

---
+++ constants/data.ts
@@ -0,0 +1,8 @@
+import {icons} from './icons'
+
+export const tabs = [
+  {name: 'index', title: "Home", icon: icons.home},
+  {name: 'Subscriptions', icon: icons.wallet},
+  {name: 'Insights', icon: icons.activity},
+  {name: 'Settings', icon: icons.setting},
+]
